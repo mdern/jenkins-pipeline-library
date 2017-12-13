fbfe593d4062cb9b17e6a4cc0e423eebd8c4e2be
Justice London <justice.m.london@disney.com>
2017-12-13 09:53:04 -0800

    Add back debug and wait

--- vars/helmDeployment.groovy
+++ vars/helmDeployment.groovy
@@ -111,7 +111,7 @@ resources:
           unstash "helmconfig"
           sh "cat values.yaml"
           
-          sh "helm upgrade testing123 chart --install --namespace development -f values.yaml"
+          sh "helm upgrade testing123 chart --debug --wait --install --namespace development -f values.yaml"
 
         }
       }
